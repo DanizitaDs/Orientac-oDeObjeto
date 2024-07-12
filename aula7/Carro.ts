2f8ff97680115d8dfe1d6b352f130caffa0f3741
DanizitaDs <162368805+DanizitaDs@users.noreply.github.com>
2024-07-12 17:12:45 -0300

    Update Carro.ts

--- aula7/Carro.ts
+++ aula7/Carro.ts
@@ -1,4 +1,5 @@
 var leitor = require('readline-sync')
+
 export class Carro {
     Tanque: number
     KM: number
@@ -10,9 +11,9 @@ export class Carro {
         this.Marca = Marca
     }
 
-
+// esse metodo serve para calcular a viagem em relacao a gasolina
     andar():void {
-        let distancia = leitor.question('Qual a Distancia que o seu carro vai percorrer? ')
+        let distancia = leitor.questionInt('Qual a Distancia que o seu carro vai percorrer? ')
         let combustivel = this.Tanque - (this.KM / distancia)
         if (this.Tanque >= combustivel){
             this.Tanque = combustivel
@@ -21,20 +22,24 @@ export class Carro {
                 console.log('Voce nao tem combustivel suficiente para andar')
         }
     }
-
+// esse metodo serve para mostrar todas as infromacoes do codigo
+    getCarro():void {
+        console.log(`Marca: ${this.Marca}, Tanque: ${this.Tanque}, km: ${this.KM}`)
+    }
+// esse metodo serve para mostrar quanto possui de gasolina
     obterGasolina(){
         console.log(`Voce atualmente possui ${this.Tanque} de Gasolina.`)
     }
- 
+ // esse metodo serve para adicionar gasolina
     adicionarGasolina(){
-        let adicionar = leitor.question('Quanto de Gasolina voce deseja adicionar? ')
+        let adicionar = leitor.questionInt('Quanto de Gasolina voce deseja adicionar? ')
         this.Tanque += adicionar
         console.log(`Voce possui atualmente ${this.Tanque} de gasolina. `)    
     }
-   
+   // esse codigo serve para cadastrar 10 carros no sistema
     Carro10(): void{
-        let tanque = leitor.question('Quanto de tanque o seu carrinho consegue consumir? ')
-        let KM = leitor.question(`Quanto de KM o seu carrinho vai percorrer? `)
+        let tanque = leitor.questionInt('Quanto de tanque o seu carrinho consegue consumir? ')
+        let KM = leitor.questionInt(`Quanto de KM o seu carrinho vai percorrer? `)
         let marca = leitor.question(`Qual a marca do seu carrinho? `)
         this.Tanque = tanque
         this.KM = KM
@@ -47,7 +52,7 @@ export class Carro {
 class Garagem{
     
 carros: Carro[] = []
-
+// esse metodo é continuação do metodo acima
     addCarro(carro: Carro):void{
         if(this.carros.length > 9){
             console.log('A garagem esta cheia')
@@ -55,6 +60,7 @@ carros: Carro[] = []
             this.carros.push(carro)
         }
     }
+// esse metodo serve para remover um carro da garagem
     remover():void {
         let nomeremovido = leitor.question("qual Carro deseja remover")
         this.carros = this.carros.filter(carro => carro.Marca !== nomeremovido)
